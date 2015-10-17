b02f4bc2b87c053ffa0825d98ebfa44dcce82f44
gireesh <m.gireesh05@gmail.com>
2015-10-17 10:05:41 +0530

    accepted solution

--- add-two-numbers/src/com/mgireesh/App.java
+++ add-two-numbers/src/com/mgireesh/App.java
@@ -5,14 +5,14 @@ public class App {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		ListNode lTemp;
-		ListNode l1 = new ListNode(2);
-		l1.next = new ListNode(4);
-		l1.next.next = new ListNode(3);
+		ListNode l1 = new ListNode(9);
+		l1.next = new ListNode(1);
+		l1.next.next = new ListNode(6);
 
-		ListNode l2 = new ListNode(5);
-		l2.next = new ListNode(6);
-		l2.next.next = new ListNode(4);
-		l2.next.next.next = new ListNode(5);
+		ListNode l2 = new ListNode(0);
+		l2.next = new ListNode(9);
+		l2.next.next = new ListNode(9);
+		l2.next.next.next = new ListNode(9);
 
 		lTemp = l1;
 		while (lTemp != null) {
--- add-two-numbers/src/com/mgireesh/Solution.java
+++ add-two-numbers/src/com/mgireesh/Solution.java
@@ -6,22 +6,86 @@ package com.mgireesh;
  */
 public class Solution {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode lOut;
-		ListNode lTemp;
+		ListNode lOut = null;
+		ListNode lOutTemp = null;
+		ListNode l1Temp = l1;
+		ListNode l2Temp = l2;
+		ListNode lTemp = null;
+		Integer sum = 0;
+		Integer carry = 0;
 
-		lTemp = l1;
-		while (lTemp != null) {
-			// System.out.printf("%d ", lTemp.val);
-			lTemp = lTemp.next;
+		if ((null == l1) && (null == l2)) {
+			System.out.printf("Both operands null");
+			return null;
 		}
 
-		// System.out.println();
-		lTemp = l2;
-		while (lTemp != null) {
-			// System.out.printf("%d ", lTemp.val);
-			lTemp = lTemp.next;
+		if ((null == l1) || (null == l2)) {
+			if (null == l1) {
+				return l2;
+			} else {
+				return l1;
+			}
 		}
 
-		return l1;
+		while (true) {
+			if (null == l1Temp) {
+				break;
+			}
+
+			if (null == l2Temp) {
+				break;
+			}
+
+			sum = l1Temp.val + l2Temp.val + carry;
+			if (sum >= 10) {
+				sum = sum - 10;
+				carry = 1;
+			} else {
+				carry = 0;
+			}
+
+			if (null == lOut) {
+				lOut = new ListNode(sum);
+				lOutTemp = lOut;
+			} else {
+				lOutTemp.next = new ListNode(sum);
+				lOutTemp = lOutTemp.next;
+			}
+
+			l1Temp = l1Temp.next;
+			l2Temp = l2Temp.next;
+		}
+
+		if ((null != l1Temp) || (null != l2Temp)) {
+			if (null != l2Temp) {
+				lTemp = l2Temp;
+			} else {
+				lTemp = l1Temp;
+			}
+
+			while (null != lTemp) {
+				if (0 != carry) {
+					sum = lTemp.val + carry;
+					if (sum >= 10) {
+						sum = sum - 10;
+						carry = 1;
+					} else {
+						carry = 0;
+					}
+					lOutTemp.next = new ListNode(sum);
+					lOutTemp = lOutTemp.next;
+				} else {
+					lOutTemp.next = lTemp;
+					break;
+				}
+				lTemp = lTemp.next;
+			}
+		}
+
+		if (0 != carry) {
+			lOutTemp.next = new ListNode(carry);
+		}
+
+		return lOut;
 	}
 }
