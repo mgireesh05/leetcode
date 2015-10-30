f096a7b37b9a39a6138094bfe5e8d6669c261e51
gireesh <m.gireesh05@gmail.com>
2015-10-30 17:14:24 +0530

    Extended solution to find the longest non repeating substring

--- longest-substring-without-repeating-characters/src/com/mgireesh/Solution.java
+++ longest-substring-without-repeating-characters/src/com/mgireesh/Solution.java
@@ -32,4 +32,38 @@ public class Solution {
 
 		return answer;
 	}
+
+	public String longestSubstring(String s) {
+
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+		if (s == null || s.length() <= 1) {
+			return s;
+		}
+
+		int rightPointer = 0;
+		int leftPointer = rightPointer - 1;
+		int answer = 0;
+		int start = 0;
+		int index = 0;
+
+		while (rightPointer != s.length()) {
+			Integer previousOccurrence = map.put(s.charAt(rightPointer), rightPointer);
+
+			if (previousOccurrence != null) {
+				leftPointer = Math.max(leftPointer, previousOccurrence);
+			}
+			answer = Math.max(answer, rightPointer - leftPointer);
+
+			if ((start + answer) != index) {
+				start = index - (answer - 1);
+			}
+
+			rightPointer++;
+			index++;
+		}
+
+		return s.substring(start, start + answer);
+	}
+
 }
