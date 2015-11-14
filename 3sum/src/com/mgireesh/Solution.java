5df724b69ff3ddaeb8d8aa4e6a4f0ebaffe7aa32
gireesh <m.gireesh05@gmail.com>
2015-11-14 19:28:05 +0530

    Better solution, but time limit exceeded again

--- 3sum/src/com/mgireesh/Solution.java
+++ 3sum/src/com/mgireesh/Solution.java
@@ -10,16 +10,18 @@ public class Solution {
 		int prev_i = 1;
 		int prev_j = 2;
 		int prev_k = 3;
+		int find = 10;
 
 		if (nums.length < 3) {
 			return answer;
 		}
 
 		Arrays.sort(nums);
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				for (int k = j + 1; k < nums.length; k++) {
-					if ((nums[i] + nums[j] + nums[k]) == 0) {
+		for (int i = 0; i < nums.length - 2; i++) {
+			for (int j = i + 1; j < nums.length - 1; j++) {
+				find = -(nums[i] + nums[j]);
+				for (int k = nums.length - 1; k > j; k--) {
+					if (nums[k] == find) {
 						List<Integer> solSet = new LinkedList<Integer>();
 						if (!((prev_i == nums[i]) && (prev_j == nums[j]) && (prev_k == nums[k]))) {
 							solSet.add(nums[i]);
@@ -29,8 +31,9 @@ public class Solution {
 							prev_i = nums[i];
 							prev_j = nums[j];
 							prev_k = nums[k];
-							break;
 						}
+					} else if (find > nums[k]) {
+						break;
 					}
 				}
 			}
