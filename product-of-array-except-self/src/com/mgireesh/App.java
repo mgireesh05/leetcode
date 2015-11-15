1aee8a5b1ed90a1a80eede007551c9a2de2496be
gireesh <m.gireesh05@gmail.com>
2015-11-15 11:13:24 +0530

    Accpeted solution; space optimized

--- product-of-array-except-self/src/com/mgireesh/App.java
+++ product-of-array-except-self/src/com/mgireesh/App.java
@@ -4,13 +4,14 @@ public class App {
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
-		int[] nums = { 1, 2, 3, 4 };
+		int[] nums = { 2, 3, 4, 5 };
 		for (int i = 0; i < nums.length; i++) {
 			System.out.printf("%d ", nums[i]);
 		}
 		System.out.println();
 
-		nums = sol.productExceptSelf(nums);
+		// nums = sol.productExceptSelf(nums);
+		nums = sol.productExceptSelfSpaceOptimized(nums);
 		for (int i = 0; i < nums.length; i++) {
 			System.out.printf("%d ", nums[i]);
 		}
--- product-of-array-except-self/src/com/mgireesh/Solution.java
+++ product-of-array-except-self/src/com/mgireesh/Solution.java
@@ -21,4 +21,21 @@ public class Solution {
 
 		return nums;
 	}
+
+	public int[] productExceptSelfSpaceOptimized(int[] nums) {
+		int[] prod = new int[nums.length];
+		int temp = 1;
+		for (int i = 0; i < nums.length; i++) {
+			prod[i] = temp;
+			temp *= nums[i];
+		}
+
+		temp = 1;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			prod[i] *= temp;
+			temp *= nums[i];
+		}
+
+		return prod;
+	}
 }
