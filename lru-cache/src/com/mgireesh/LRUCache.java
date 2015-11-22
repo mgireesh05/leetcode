5f29ffb2e70a2f58022c16bd2d69a13c0a3250f7
gireesh <m.gireesh05@gmail.com>
2015-11-22 18:42:09 +0530

    Accepted solution; replaced SLL with DLL

--- lru-cache/src/com/mgireesh/LRUCache.java
+++ lru-cache/src/com/mgireesh/LRUCache.java
@@ -1,12 +1,59 @@
 package com.mgireesh;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class LRUCache {
 	private int cap;
-	private HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-	private LinkedList<Integer> list = new LinkedList<Integer>();
+	private ListNode head = null;
+	private ListNode tail = null;
+	private HashMap<Integer, ListNode> map = new HashMap<Integer, ListNode>();
+
+	class ListNode {
+		int key;
+		int val;
+		ListNode next;
+		ListNode prev;
+
+		ListNode(int x, int y) {
+			key = x;
+			val = y;
+		}
+	}
+
+	public void addToHead(ListNode node) {
+		if (node == null) {
+			return;
+		}
+
+		if ((head == null) || (tail == null)) {
+			head = tail = node;
+		} else {
+			node.next = head;
+			head.prev = node;
+			head = node;
+		}
+	}
+
+	public void removeNode(ListNode node) {
+		if (node == null) {
+			return;
+		}
+
+		if (node.prev != null) {
+			node.prev.next = node.next;
+		} else {
+			head = head.next;
+		}
+
+		if (node.next != null) {
+			node.next.prev = node.prev;
+		} else {
+			tail = node.prev;
+		}
+
+		node.next = null;
+		node.prev = null;
+	}
 
 	public LRUCache(int capacity) {
 		cap = capacity;
@@ -14,27 +61,34 @@ public class LRUCache {
 
 	public int get(int key) {
 		if (map.containsKey(key)) {
-			list.removeFirstOccurrence(key);
-			list.addFirst(key);
-			return map.get(key);
-		} else {
-			return -1;
+			ListNode node = map.get(key);
+			removeNode(node);
+			addToHead(node);
+			return node.val;
 		}
+		return -1;
 	}
 
 	public void set(int key, int value) {
 		if (map.containsKey(key)) {
-			map.put(key, value);
-			return;
-		}
-		if (map.size() < cap) {
-			map.put(key, value);
-			list.addLast(key);
+			ListNode node = map.get(key);
+			node.val = value;
+			map.put(key, node);
+			removeNode(node);
+			addToHead(node);
 		} else {
-			Integer i = list.removeLast();
-			map.remove(i);
-			map.put(key, value);
-			list.addLast(key);
+
+			ListNode node = new ListNode(key, value);
+			if (map.size() < cap) {
+				addToHead(node);
+			} else {
+				map.remove(tail.key);
+				removeNode(tail);
+				node.key = key;
+				node.val = value;
+				addToHead(node);
+			}
+			map.put(key, node);
 		}
 	}
 }
