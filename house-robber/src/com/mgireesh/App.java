b5e7765a887bbf37832c430170800834456ee18b
gireesh <m.gireesh05@gmail.com>
2015-12-14 21:59:53 +0530

    Accepted solution

---
+++ house-robber/src/com/mgireesh/App.java
@@ -0,0 +1,12 @@
+package com.mgireesh;
+
+public class App {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = { 50, 10, 20, 100 };
+		System.out.println(sol.rob(nums));
+
+	}
+
+}
