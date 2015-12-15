18432e9c0d28ef22e4d447d785ecc36d949a8892
gireesh <m.gireesh05@gmail.com>
2015-12-15 18:39:11 +0530

    Accepted solution

--- climbing-stairs/src/com/mgireesh/App.java
+++ climbing-stairs/src/com/mgireesh/App.java
@@ -4,11 +4,22 @@ public class App {
 
 	public static void main(String[] args) {
 		Solution sol = new Solution();
+		System.out.println(sol.climbStairsRecursive(1));
+		System.out.println(sol.climbStairsRecursive(3));
+		System.out.println(sol.climbStairsRecursive(4));
+		System.out.println(sol.climbStairsRecursive(5));
+		System.out.println(sol.climbStairsRecursive(10));
+		System.out.println(sol.climbStairsRecursive(44));
+
+		System.out.println();
+
+		System.out.println(sol.climbStairs(1));
 		System.out.println(sol.climbStairs(3));
 		System.out.println(sol.climbStairs(4));
 		System.out.println(sol.climbStairs(5));
 		System.out.println(sol.climbStairs(10));
 		System.out.println(sol.climbStairs(44));
+
 	}
 
 }
--- climbing-stairs/src/com/mgireesh/Solution.java
+++ climbing-stairs/src/com/mgireesh/Solution.java
@@ -1,11 +1,26 @@
 package com.mgireesh;
 
 public class Solution {
-	public int climbStairs(int n) {
+	public int climbStairsRecursive(int n) {
 		if (n <= 2) {
 			return n;
 		}
 
-		return climbStairs(n - 1) + climbStairs(n - 2);
+		return climbStairsRecursive(n - 1) + climbStairsRecursive(n - 2);
 	}
+
+	// fibonacci series
+	public int climbStairs(int n) {
+		int a = 1;
+		int b = 1;
+		int sum = 1;
+		for (int i = 2; i <= n; i++) {
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+
+		return sum;
+	}
+
 }
