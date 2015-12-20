dd6798a9d5da92eec32eb7cb58a4e0a3c6ec9d01
gireesh <m.gireesh05@gmail.com>
2015-12-20 14:12:24 +0530

    Accepted solution - O(n)

---
+++ find-peak-element/src/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = { 1, 2, 3, 1 };
+		// int[] nums = { 1 };
+		System.out.println(sol.findPeakElement(nums));
+
+	}
+
+}
---
+++ find-peak-element/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Solution {
+	public int findPeakElement(int[] nums) {
+
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int peak = nums[0];
+		map.put(peak, 0);
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > peak) {
+				peak = nums[i];
+				map.put(peak, i);
+			}
+		}
+		return map.get(peak);
+	}
+}
