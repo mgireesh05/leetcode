f6775f2c81eeb253fc519495cb9e0c5504cd0fd2
gireesh <m.gireesh05@gmail.com>
2015-12-20 19:57:34 +0530

    Accepted solution

---
+++ contains-duplicate-ii/src/com/mgireesh/App.java
@@ -0,0 +1,12 @@
+package com.mgireesh;
+
+public class App {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = { 1, 2, 3, 4, 2 };
+		System.out.println(sol.containsNearbyDuplicate(nums, 3));
+
+	}
+
+}
