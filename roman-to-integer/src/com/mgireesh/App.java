0da5f19b2aaed1260703019c96961216147f5168
gireesh <m.gireesh05@gmail.com>
2015-12-20 20:29:39 +0530

    Accepted solution

---
+++ roman-to-integer/src/com/mgireesh/App.java
@@ -0,0 +1,14 @@
+package com.mgireesh;
+
+public class App {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.romanToInt("MCMLIV"));
+		System.out.println(sol.romanToInt("MCMXC"));
+		System.out.println(sol.romanToInt("M"));
+		System.out.println(sol.romanToInt("IM"));
+
+	}
+
+}
---
+++ roman-to-integer/src/com/mgireesh/Solution.java
@@ -0,0 +1,26 @@
+package com.mgireesh;
+
+import java.util.HashMap;
+
+public class Solution {
+	public int romanToInt(String s) {
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if ((i + 1 < s.length()) && (map.get(s.charAt(i)) < map.get(s.charAt(i + 1)))) {
+				sum += map.get(s.charAt(i + 1)) - map.get(s.charAt(i));
+				i++;
+			} else {
+				sum += map.get(s.charAt(i));
+			}
+		}
+		return sum;
+	}
+}
