7529e7603c1414270734a9dec492b22331e46ffe
gireesh <m.gireesh05@gmail.com>
2016-01-17 19:40:18 +0530

    Accepted solution

---
+++ power-of-three/src/com/mgireesh/App.java
@@ -0,0 +1,12 @@
+package com.mgireesh;
+
+public class App {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.isPowerOfThree(243));
+		System.out.println(sol.isPowerOfThree(4));
+		System.out.println(sol.isPowerOfThree(0));
+	}
+
+}
---
+++ power-of-three/src/com/mgireesh/Solution.java
@@ -0,0 +1,10 @@
+package com.mgireesh;
+
+public class Solution {
+	public boolean isPowerOfThree(int n) {
+		while (n != 0 && n % 3 == 0) {
+			n /= 3;
+		}
+		return (n == 1);
+	}
+}
