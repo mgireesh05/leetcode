7434df196f26b4f7982acb354f1266cbc7f14108
gireesh <m.gireesh05@gmail.com>
2016-03-06 08:33:56 +0530

    Accepted solution

--- rotate-array/src/com/mgireesh/App.java
+++ rotate-array/src/com/mgireesh/App.java
@@ -5,11 +5,11 @@ public class App {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
-		// int[] nums = { 1 };
+		// int[] nums = { 1, 2 };
 		for (int i : nums) {
 			System.out.printf("%d ", i);
 		}
-		sol.rotate(nums, 1);
+		sol.rotate(nums, 3);
 		System.out.println();
 		for (int i : nums) {
 			System.out.printf("%d ", i);
--- rotate-array/src/com/mgireesh/Solution.java
+++ rotate-array/src/com/mgireesh/Solution.java
@@ -1,27 +1,22 @@
 package com.mgireesh;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Solution {
 	public void rotate(int[] nums, int k) {
-		if (k < nums.length) {
-			for (int i = nums.length - k, j = 0; i < nums.length; i++, j++) {
-				int temp = nums[j];
-				nums[j] = nums[i];
-				nums[i] = temp;
-			}
-
-			for (int i = 0; i < k; i++) {
-				rotateOnce(nums, k);
-			}
+		Deque<Integer> dq = (Deque<Integer>) new LinkedList<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			dq.add(nums[i]);
 		}
-	}
 
-	private void rotateOnce(int[] nums, int start) {
-		if (start < nums.length) {
-			int temp = nums[nums.length - 1];
-			for (int i = nums.length - 1; i >= start; i--) {
-				nums[i] = nums[i - 1];
-			}
-			nums[start] = temp;
+		for (int i = 0; i < k; i++) {
+			int val = dq.removeLast();
+			dq.push(val);
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = dq.pop();
 		}
 	}
 }
