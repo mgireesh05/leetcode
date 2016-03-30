36c5a30338caf5e08bb7d96c01921d7fd92b4921
gireesh <m.gireesh05@gmail.com>
2016-03-30 16:15:39 +0530

    Accepted solution

---
+++ factorial-trailing-zeroes/src/com/mgireesh/App.java
@@ -0,0 +1,16 @@
+package com.mgireesh;
+
+public class App {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.trailingZeroes(2));
+		System.out.println(sol.trailingZeroes(5));
+		System.out.println(sol.trailingZeroes(7));
+		System.out.println(sol.trailingZeroes(10));
+		System.out.println(sol.trailingZeroes(15));
+		System.out.println(sol.trailingZeroes(20));
+		System.out.println(sol.trailingZeroes(25));
+	}
+
+}
---
+++ factorial-trailing-zeroes/src/com/mgireesh/Solution.java
@@ -0,0 +1,11 @@
+package com.mgireesh;
+
+public class Solution {
+
+	public int trailingZeroes(int n) {
+		if (n < 5) {
+			return 0;
+		}
+		return n / 5 + trailingZeroes(n / 5);
+	}
+}
